9c7bf28e426122cd060d0a760299edac48cba503
jtlewis81 <jtlewis91481@gmail.com>
2023-03-25 20:35:56 -0400

    final bug fixes for 3/25/23

--- src/main/java/com/gcu/cst326clc/data/ProductDataService.java
+++ src/main/java/com/gcu/cst326clc/data/ProductDataService.java
@@ -102,36 +102,21 @@ public class ProductDataService implements DataAccessInterface<ProductModel> {
 
 	public List<ProductModel> findByNameContainingIgnoreCase(String query)
 	{		
-		String sql = "SELECT * FROM PRODUCT WHERE PRODUCT_NAME LIKE ?";
-		String sql2 = "SELECT * FROM PRODUCT WHERE PRODUCT_DESCRIPTION LIKE ?";
-		Object[] args = { "%" + query + "%" };
-		 List<ProductModel> products = new ArrayList<>();
+		String sql = "SELECT * FROM PRODUCT WHERE PRODUCT_NAME LIKE ? OR PRODUCT_DESCRIPTION LIKE ?";
+		List<ProductModel> products = new ArrayList<>();
 	        try {
-	            SqlRowSet srs = jdbcTemplate.queryForRowSet(sql, args);
+	        
+	            SqlRowSet srs = jdbcTemplate.queryForRowSet(sql, "%" + query + "%", "%" + query + "%");
 	            while (srs.next()) {
 	                products.add(new ProductModel(srs.getInt("PRODUCT_ID"),
 	                        srs.getString("PRODUCT_NAME"),
 	                        srs.getString("PRODUCT_DESCRIPTION"),
 	                        srs.getFloat("PRODUCT_PRICE"),
 	                        srs.getInt("PRODUCT_QUANTITY")));
-	                
-	                try {
-	    	            SqlRowSet srs2 = jdbcTemplate.queryForRowSet(sql2, args);
-	    	            while (srs2.next()) {
-	    	                products.add(new ProductModel(srs2.getInt("PRODUCT_ID"),
-	    	                		srs2.getString("PRODUCT_NAME"),
-	    	                		srs2.getString("PRODUCT_DESCRIPTION"),
-	    	                		srs2.getFloat("PRODUCT_PRICE"),
-	    	                		srs2.getInt("PRODUCT_QUANTITY")));
-	    	            }
-	    	        } catch (Exception e) {
-	    	            e.printStackTrace();
-	    	        }
 	            }
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        }
 	        return products;
-		
 	}
 }
