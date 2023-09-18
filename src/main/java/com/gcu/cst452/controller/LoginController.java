a6b52bd018fbfe4f0da1a83c59713beff6234ccd
cylon-raider <markel74@msn.com>
2023-09-17 21:33:48 -0700

    miscellaneous changes

--- src/main/java/com/gcu/cst452/controller/LoginController.java
+++ src/main/java/com/gcu/cst452/controller/LoginController.java
@@ -1,12 +1,23 @@
 package com.gcu.cst452.controller;
 
+import com.gcu.cst452.business.UserBusinessService;
 import com.gcu.cst452.model.LoginModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserBusinessService userBusinessService;
 
     @GetMapping("/")
     public ModelAndView display(){
@@ -16,5 +27,19 @@ public class LoginController {
         modelAndView.addObject("loginModel", loginModel);
         modelAndView.setViewName("login");
         return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loginUser(@ModelAttribute LoginModel loginModel, BindingResult bindingResult, Model model) {
+        try {
+
+            UserDetails userDetails = userBusinessService.loadUserByUsername(loginModel.getUsername());
+
+            return new ModelAndView("redirect:/");
+        } catch (UsernameNotFoundException e) {
+            model.addAttribute("errorMessage", "User not registered. Please sign up.");
+            return display(); // Redirect back to the login page with an error message
+        }
+
     }
 }
